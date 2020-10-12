3eccad51ab81f397a4d1f1f8d280ca73a99b5687
GemaPujalte <gema.pujalte.iglesias@gmail.com>
2020-10-12 14:58:04 +0200

    add code js optimization

--- src/js/main.js
+++ src/js/main.js
@@ -6,49 +6,69 @@ const inputElementMedium = document.querySelector(".js-inputMedium");
 
 const name = document.querySelector(".js-text-name");
 const work = document.querySelector(".js-text-work");
-const icons = document.querySelectorAll(".js-icons");
+const previewBorder = document.querySelector(".js-preview__card--text");
 
+const icons = document.querySelectorAll(".js-icons");
 const socialNetworks = document.querySelectorAll(".js-socialNetworks");
 
 function changeCold() {
-  name.classList.add("js-paletteColdA");
-  work.classList.add("js-paletteColdB");
-
-  for (const icon of icons) {
-    icon.classList.add("js-paletteColdC");
-  }
-
-  for (const network of socialNetworks) {
-    network.classList.add("js-paletteColdA");
-  }
+  paintName("js-paletteColdA", "js-paletteWarmA", "js-paletteMediumA");
+  paintWork("js-paletteColdB", "js-paletteWarmB", "js-paletteMediumB");
+  paintBorder("js-paletteColdB", "js-paletteWarmB", "js-paletteMediumB");
+  paintIcons("js-paletteColdC", "js-paletteWarmC", "js-paletteMediumC");
+  paintNetworks("js-paletteColdA", "js-paletteWarmA", "js-paletteMediumA");
 }
 
 function changeWarm() {
-  name.classList.add("js-paletteWarmA");
-  work.classList.add("js-paletteWarmB");
-
-  for (const icon of icons) {
-    icon.classList.add("js-paletteWarmC");
-  }
-
-  for (const network of socialNetworks) {
-    network.classList.add("js-paletteWarmA");
-  }
+  paintName("js-paletteWarmA", "js-paletteColdA", "js-paletteMediumA");
+  paintWork("js-paletteWarmB", "js-paletteColdB", "js-paletteMediumB");
+  paintBorder("js-paletteWarmB", "js-paletteColdB", "js-paletteMediumB");
+  paintIcons("js-paletteWarmC", "js-paletteColdC", "js-paletteMediumC");
+  paintNetworks("js-paletteWarmA", "js-paletteColdA", "js-paletteMediumA");
 }
 
 function changeMedium() {
-  name.classList.add("js-paletteMediumA");
-  work.classList.add("js-paletteMediumB");
-
-  for (const icon of icons) {
-    icon.classList.add("js-paletteMediumC");
-  }
-
-  for (const network of socialNetworks) {
-    network.classList.add("js-paletteMediumA");
-  }
+  paintName("js-paletteMediumA", "js-paletteColdA", "js-paletteWarmA");
+  paintWork("js-paletteMediumB", "js-paletteColdB", "js-paletteWarmB");
+  paintBorder("js-paletteMediumB", "js-paletteColdB", "js-paletteWarmB");
+  paintIcons("js-paletteMediumC", "js-paletteColdC", "js-paletteWarmC");
+  paintNetworks("js-paletteMediumA", "js-paletteColdA", "js-paletteWarmA");
 }
 
 inputElementCold.addEventListener("click", changeCold);
 inputElementWarm.addEventListener("click", changeWarm);
 inputElementMedium.addEventListener("click", changeMedium);
+
+function paintName(classToAdd, classToRemove1, classToRemove2) {
+  name.classList.add(classToAdd);
+  name.classList.remove(classToRemove1);
+  name.classList.remove(classToRemove2);
+}
+
+function paintWork(classToAdd, classToRemove1, classToRemove2) {
+  work.classList.add(classToAdd);
+  work.classList.remove(classToRemove1);
+  work.classList.remove(classToRemove2);
+}
+
+function paintBorder(classToAdd, classToRemove1, classToRemove2) {
+  previewBorder.classList.add(classToAdd);
+  previewBorder.classList.remove(classToRemove1);
+  previewBorder.classList.remove(classToRemove2);
+}
+
+function paintIcons(classToAdd, classToRemove1, classToRemove2) {
+  for (const icon of icons) {
+    icon.classList.add(classToAdd);
+    icon.classList.remove(classToRemove1);
+    icon.classList.remove(classToRemove2);
+  }
+}
+
+function paintNetworks(classToAdd, classToRemove1, classToRemove2) {
+  for (const network of socialNetworks) {
+    network.classList.add(classToAdd);
+    network.classList.remove(classToRemove1);
+    network.classList.remove(classToRemove2);
+  }
+}
