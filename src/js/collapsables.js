57580c1e11eed6a6f654f59b1cbbccd80a5956ea
BMatillaVaras <bmatillavaras@gmail.com>
2020-10-15 14:02:04 +0200

    modifi collapsables js

--- src/js/collapsables.js
+++ src/js/collapsables.js
@@ -2,29 +2,29 @@
 "use strict";
 
 //listener de arrow
-const arrowEls = document.querySelectorAll(".fa-chevron-up"); //puede ser cualquiera de los 3 arrows.
-
+const arrowEls = document.querySelectorAll(".js-collapsable-title"); //puede ser cualquiera de los 3 arrows.
 const clickArrow = function (event) {
   //deberemos deferenciar qué sección ha sido clickada y si pliega o despliega. Según clases activas..
-  const element = event.target;
-
-  if (element.classList.contains("design__title--arrow")) {
+  const element = event.currentTarget;
+  if (element.classList.contains("design__title")) {
     // console.log('entro en collapsable DESIGN');
     const section = document.querySelector(".design__form");
-    element.classList.toggle("js-fa-chevron-up");
+    const arrow = element.querySelector(".fa-chevron-up");
+    arrow.classList.toggle("js-fa-chevron-up");
     section.classList.toggle("js-design__form");
   }
-  if (element.classList.contains("fill__title--arrow")) {
+  if (element.classList.contains("fill__title")) {
     // console.log('entro en collapsable rellena');
     const section = document.querySelector(".fill__form");
-    element.classList.toggle("js-fa-chevron-up");
+    const arrow = element.querySelector(".fa-chevron-up");
+    arrow.classList.toggle("js-fa-chevron-up");
     section.classList.toggle("js-fill__form");
   }
-  if (element.classList.contains("share__title--arrow")) {
+  if (element.classList.contains("share__title")) {
     const section = document.querySelector(".share__section");
     const shareEl = document.querySelector(".share");
-
-    element.classList.toggle("js-fa-chevron-up");
+    const arrow = element.querySelector(".fa-chevron-up");
+    arrow.classList.toggle("js-fa-chevron-up");
     section.classList.toggle("js-share__section");
     shareEl.classList.toggle("js-share");
   }
